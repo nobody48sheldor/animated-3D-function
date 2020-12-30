aaafcd3b1f621739537d55271142825493495326
nobody48sheldor <69438501+nobody48sheldor@users.noreply.github.com>
2020-12-30 22:22:12 +0100

    Create Animated-3D-function.py

---
+++ Animated-3D-function.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+a = int(input("render level = "))
+tmax = float(input("tmax = "))
+xmax = float(input("xmax = "))
+ymax = float(input("ymax = "))
+
+X = np.linspace(0, xmax, a)
+Y = np.linspace(0, ymax, a)
+
+x, y = np.meshgrid(X, Y)
+
+t = np.linspace(0, tmax, a)
+
+def f(x, y, t):
+    s = #your function
+    return(s)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+plt.ion()
+
+i = 0
+
+while i<a:
+    z = f(x, y, t[i])
+    plt.clf()
+    ax = fig.add_subplot(111, projection = '3d')
+    plt.title('Time = {} secondes'.format(t[i]))
+    ax.plot_surface(x, y, f(x, y, t[i]), cmap = cm.plasma, linewidth=0, antialiased=True)
+    ax.axes.set_xlim3d(left=0, right=xmax)
+    ax.axes.set_ylim3d(bottom=0, top=ymax)
+    ax.axes.set_zlim3d(bottom=-1.2, top=1.2)
+    plt.pause(1/(a*tmax))
+    i = i + 1
